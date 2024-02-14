088888694ecfa6f140fa86827b99b44b182249ae
tharindu432 <tharinduchaturanga432@gmail.com>
2024-02-15 02:26:27 +0530

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,18 @@ const Navbar = () => {
   return (
     <div>
       <header className='bg-dark'> 
-        <nav className='px-4 py-4'>
+        <nav className='d-flex align-items-center px-4 py-4 justify-content-between'>
 
-       <Link to='/' className='text-white h1 font-weight-bold text-decoration-none'><span className='text-warning'>T</span>-BLOG</Link>
-    
+       <Link to='/' className='text-white h1 font-weight-bold text-decoration-none'><span className='text-warning'>T</span>-Blog</Link>
+       
+       <ul>
+         {navItems.map((item, index) => (
+            <Link key={index} to={item.path} className='text-white text-decoration-none mx-3'>
+                {item.link}
+            </Link>
+        ))}
+       </ul>
+
         </nav>
       </header>
     </div>
