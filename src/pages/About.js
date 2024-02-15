31cdde139ed8fee0f3037ea3ed0d4828855beeda
tharindu432 <tharinduchaturanga432@gmail.com>
2024-02-15 18:45:40 +0530

    our team section added

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,13 +8,18 @@ import Lottie from 'react-lottie';
 import TeamMemberCard from '../components/TeamMemberCard';
 
 //teame member images
-import member1 from '../assets/about.png'
+import member1 from '../assets/member1.jpg';
+import member2 from '../assets/member2.jpg'
+import member3 from '../assets/member3.jpg'
+import member4 from '../assets/member4.jpg'
+import member5 from '../assets/member5.jpg'
  
 
 const About = () => {
   const animationData = require('../assets/about.json'); 
   return (
     <div className="container-fluid px-5 pt-5 pb-3 bg-dark text-white">
+      <br></br>
       <div className="row row-cols-1 row-cols-md-2 g-4 align-items-center min-vh-100">
         <div className="col">
           <h1 className="display-4 fw-bold">About Us</h1>
@@ -31,9 +36,13 @@ const About = () => {
           <Lottie options={{ animationData, loop: true }} style={{ width: '45vw' }} />
           
         </div>
+        </div>
+        <div>
+        <h1 className="display-3 fw-bold">Our Team</h1>
+        </div>
 
-          <h1 className="display-4 fw-bold">Our Team</h1>
-          <div className="row row-cols-1 row-cols-md-3 g-4">
+        
+          <div className="row row-cols-2 row-cols-md-5 g-4">
             <TeamMemberCard
               name="John Doe"
               title="CEO"
@@ -43,7 +52,7 @@ const About = () => {
             <TeamMemberCard
               name="Jane Doe"
               title="CTO"
-              image={member1}
+              image={member2}
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Integer nec odio praesent libero mollis interdum blandit eget dolor."
             />
 
@@ -51,15 +60,22 @@ const About = () => {
             <TeamMemberCard
               name="John Smith"
               title="COO"
-              image={member1}
+              image={member3}
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Integer nec odio praesent libero mollis interdum blandit eget dolor."
             />
             <TeamMemberCard
               name="Jane Smith"
               title="CFO"
-              image={member1}
+              image={member4}
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Integer nec odio praesent libero mollis interdum blandit eget dolor."
             />
+            <TeamMemberCard
+              name="Jane Smith"
+              title="CFO"
+              image={member5}
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Integer nec odio praesent libero mollis interdum blandit eget dolor."
+            />
+
 
 
       
@@ -67,7 +83,7 @@ const About = () => {
        
       
     </div>
-  </div>
+  
   );
 };
 
